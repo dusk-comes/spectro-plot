fae6ef25270d67ffb38610b7895c71adb3339c7b
ak <14f143@gmail.com>
2023-09-03 22:52:36 +0300

    test plot in non-real time mode

--- main.cpp
+++ main.cpp
@@ -1,4 +1,19 @@
-int main()
+#include <QApplication>
+
+#include "mainwindow.hpp"
+#include "model.hpp"
+
+int main(int argc, char *argv[])
 {
-    return 0;
+    QApplication a(argc, argv);
+
+    model md("resources/test.wav", 1024U);
+    md.calculate();
+
+    MainWindow mw;
+    mw.set_map_size(md.stripes(), md.bins());
+    mw.set_map_range(md.stripes() * md.time_resolution().count(), md.bins() * md.freq_resolutions());
+    mw.set_data(md.m_data_to_display);
+    mw.show();
+    return a.exec();
 }
